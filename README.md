89392e12d4a0f98892f5307ee7631a87c5ae0fd9
Sam Forderer <samsloanforderer@icloud.com>
2023-05-05 13:01:19 -0400

    added README

---
+++ README.md
@@ -0,0 +1,10 @@
+# Bulk Email Cleaner
+
+There are two stages of email verification: 
+
+1. The sender's mail server determines the addressee's mail server using DNS service.
+2. The sender's mail server connects to the addressee's mail server through the SMTP protocol and transmits the message.
+
+## DNS Service 
+- The checking software reads the e-mail address syntactically
+- signals out the mail domain and sends a request to the DNS server to get the mail server of this domain
--- main.py
+++ main.py
@@ -1,43 +1,31 @@
 import csv
-import py3-validate-email
+import itertools
+from validate_email import validate_email
 
 
-def readfile(in_file):
+def readfile(in_file, startrow, endrow):
     # open file
     with open(f"{in_file}.csv", 'r') as csvfile:
         # define reader object
         csv_reader = csv.reader(csvfile)
-        # skip header line
-        next(csv_reader)
         # loop through
-        for line in csv_reader:
-            emailList.append(line[0])
+        for line in itertools.islice(csv_reader, startrow, endrow):
+            emailList.append(line[2])
+            print(line[2])
 
-
-def validate_email_addr(email_address):
-
-    is_valid = validate_email(
-        email_address='example@example.com',
-        check_format=True,
-        check_blacklist=True,
-        check_dns=True,
-        dns_timeout=10,
-        check_smtp=True,
-        smtp_timeout=10,
-        smtp_helo_host='my.host.name',
-        smtp_from_address='my@from.addr.ess',
-        smtp_skip_tls=False,
-        smtp_tls_context=None,
-        smtp_debug=False,
-        address_types=frozenset([IPv4Address, IPv6Address]))
-
-
+    
 def writefile(out_file):
     with open(f'{out_file}.csv', 'a', newline='') as file:
+
         writer = csv.writer(file)
-        for i in range(len(emailList)):
-            if validate_email_addr(emailList[i]):
+        for i in range(0, len(emailList)):
+
+            valid = validate_email(email_address=emailList[i])
+
+            if valid:
                 writer.writerow([f'{emailList[i]}'])
+                print(valid)
+
             else:
                 print("error validating")
 
@@ -46,14 +34,15 @@ def writefile(out_file):
 # file names for input and output
 inFileName = input("Enter file name: ")
 outFileName = input("Enter Name of output file: ")
+startRow = int(input("What row to start processing from: "))
+endRow = int(input("Enter number of rows to validate: "))
 
 # initialize list
 emailList = []
 
-print('beginning email reading')
-readfile(inFileName)
-print('email reading complete..')
 
-print('beginning email writing and validating')
-writefile(outFileName)
+# function calls
+readfile(inFileName)
+
+writefile(outFileName, startRow, endRow)
 
